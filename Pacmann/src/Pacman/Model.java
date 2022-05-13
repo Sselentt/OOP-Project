0edb97ef4418a1be2e63a1e7f36991a39a912304
Sselentt <sselentt23@gmail.com>
2022-05-14 02:30:22 +0300

    part2

--- Pacmann/src/Pacman/Model.java
+++ Pacmann/src/Pacman/Model.java
@@ -3,10 +3,15 @@ package Pacman;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.Timer;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Model extends JPanel implements ActionListener{
@@ -25,9 +30,9 @@ public class Model extends JPanel implements ActionListener{
 	private int N_GHOSTS = 8;
 	private int lives, score;
 	private int [] dx, dy;
-	private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghsotSpeed;
+	private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
 	
-	private Image heart, ghsot;
+	private Image heart, ghost;
 	private Image up, down, left, right;
 	
 	private int pacman_x, pacman_y, pacmand_x, pacmand_y;
@@ -39,7 +44,7 @@ public class Model extends JPanel implements ActionListener{
 	private short [] screenData;
 	private Timer timer;
 	
-	private final short leveData[] = {
+	private final short levelData[] = {
 			19, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 22,
 	        17, 16, 16, 16, 16, 24, 16, 16, 16, 16, 16, 16, 16, 16, 20,
 	        25, 24, 24, 24, 28, 0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
@@ -57,14 +62,141 @@ public class Model extends JPanel implements ActionListener{
 	        25, 24, 24, 24, 26, 24, 24, 24, 24, 24, 24, 24, 24, 24, 28
 	};
 	
+	public Model() {
+		loadImages();
+		ÝnitVariables();
+		addKeyListener(new TAdapter());
+		setFocusable(true);
+		initGame();
+	}
+	
+	private void loadImages() {
+		down = new ImageIcon("C://Users//Hp//OneDrive//Masaüstü//images").getImage();
+		up = new ImageIcon("C://Users//Hp//OneDrive//Masaüstü//images").getImage();
+		left = new ImageIcon("C://Users//Hp//OneDrive//Masaüstü//images").getImage();
+		right = new ImageIcon("C://Users//Hp//OneDrive//Masaüstü//images").getImage();
+		ghost = new ImageIcon("C://Users//Hp//OneDrive//Masaüstü//images").getImage();
+		heart = new ImageIcon("C://Users//Hp//OneDrive//Masaüstü//images").getImage();	
+	}
+	
+	private void ÝnitVariables() {
+		screenData = new short[N_BLOCKS * N_BLOCKS];
+		d = new Dimension(400,400);
+		ghost_x = new int [MAX_GHOSTS];
+		ghost_dx = new int [MAX_GHOSTS];
+		ghost_y = new int [MAX_GHOSTS];
+		ghost_dy = new int [MAX_GHOSTS];
+		ghostSpeed = new int [MAX_GHOSTS];
+		dx = new int[4];
+		dy = new int [4];
+		
+		timer = new Timer(40, this);
+		timer.restart();
+	}
 	
 	
+	private void initGame( ) {
+		lives = 3;
+		score = 0;
+		initLevel();
+		N_GHOSTS = 6;
+		currentSpeed = 3;
+	}
 	
+	private void initLevel() {
+		int i;
+		for (i = 0; i< N_BLOCKS * N_BLOCKS; i++) {
+			screenData[i] = levelData[i];
+		}
+	}
 	
+    private void playGame(Graphics2D g2d) {
+    	
+    	
+    	
+    }
+    
+    
+	private void continueLevel() {
+
+    	int dx = 1;
+        int random;
+
+        for (int i = 0; i < N_GHOSTS; i++) {
+
+            ghost_y[i] = 4 * BLOCK_SIZE; 
+            ghost_x[i] = 4 * BLOCK_SIZE;
+            ghost_dy[i] = 0;
+            ghost_dx[i] = dx;
+            dx = -dx;
+            random = (int) (Math.random() * (currentSpeed + 1));
+
+            if (random > currentSpeed) {
+                random = currentSpeed;
+            }
+
+            ghostSpeed[i] = validSpeeds[random];
+        }
+
+        pacman_x = 7 * BLOCK_SIZE; 
+        pacman_y = 11 * BLOCK_SIZE;
+        pacmand_x = 0;	
+        pacmand_y = 0;
+        req_dx = 0;		
+        req_dy = 0;
+        dying = false;
+    }
 	
+	public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.setColor(Color.black);
+        g2d.fillRect(0, 0, d.width, d.height);
+
+        drawMaze(g2d);
+        drawScore(g2d);
+
+        if (inGame) {
+            playGame(g2d);
+        } else {
+            showIntroScreen(g2d);
+        }
+
+        Toolkit.getDefaultToolkit().sync();
+
+	}
 	
-	
-	
+	class TAdapter extends KeyAdapter {
+        public void keyPressed(KeyEvent e) {
+           int key = e.getKeyCode();
+
+            if (inGame) {
+                if (key == KeyEvent.VK_LEFT) {
+                    req_dx = -1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    req_dx = 1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_UP) {
+                    req_dx = 0;
+                    req_dy = -1;
+                } else if (key == KeyEvent.VK_DOWN) {
+                    req_dx = 0;
+                    req_dy = 1;
+                } else if (key == KeyEvent.VK_ESCAPE && timer.isRunning()) {
+                    inGame = false;
+                } 
+            } else {
+                if (key == KeyEvent.VK_SPACE) {
+                    inGame = true;
+                    initGame();
+                }
+            }
+        }
+}
+
 	
 	
 	@Override
